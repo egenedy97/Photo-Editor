52174b0a310d6e48f367bc86fbf3b32d6b0c72c5
Eslam Genedy <eslamgenedy377@yahoo.com>
2021-05-23 14:08:56 +0200

    Create Upload for images and show it in canvas

--- editBar.py
+++ editBar.py
@@ -8,7 +8,7 @@ Created on Sun May 23 05:30:58 2021
 # =============================================================================
 # Edit Options Bar
 # =============================================================================
-from tkinter import Frame, Button, LEFT
+from tkinter import Frame, Button, LEFT ,Entry
 from tkinter import filedialog
 import cv2
 
@@ -22,15 +22,50 @@ class EditBar(Frame):
         self.save_as_button = Button(self, text="Save As")
         self.rotate_button = Button(self, text="Rotate")
         self.crop_button = Button(self, text="Crop")
-        self.filter_button = Button(self, text="Filter")
-        self.adjust_button = Button(self, text="Adjust")
+        self.flipping_button = Button(self, text="Flipping")
+        self.transform_button = Button(self, text="transformation")
         self.clear_button = Button(self, text="Clear")
+        
+        self.new_button.bind("<ButtonRelease>", self.new_button_released)
+        self.save_button.bind("<ButtonRelease>", self.save_button_released)
+        self.save_as_button.bind("<ButtonRelease>", self.save_as_button_released)
+# =============================================================================
+#         self.rotate_button.bind("<ButtonRelease>", self.rotate_button_released)
+#         self.crop_button.bind("<ButtonRelease>", self.crop_button_released)
+#         self.flipping_button.bind("<ButtonRelease>", self.flipping_button_released)
+#         self.transform_button.bind("<ButtonRelease>", self.transform_button_released)
+# =============================================================================
+        self.clear_button.bind("<ButtonRelease>", self.clear_button_released)
         
         self.new_button.pack(side=LEFT)
         self.save_button.pack(side=LEFT)
         self.save_as_button.pack(side=LEFT)
         self.rotate_button.pack(side = LEFT)
+#        self.angle.pack(side=LEFT)
         self.crop_button.pack(side=LEFT)
-        self.filter_button.pack(side=LEFT)
-        self.adjust_button.pack(side=LEFT)
+        self.flipping_button.pack(side=LEFT)
+        self.transform_button.pack(side=LEFT)
         self.clear_button.pack()
+        
+    
+    def new_button_released(self, event):
+        if self.winfo_containing(event.x_root, event.y_root) == self.new_button:
+            filename = filedialog.askopenfilename()
+            image = cv2.imread(filename)
+
+            if image is not None:
+                self.master.filename = filename
+                print(filename)
+                self.master.original_image = image.copy()
+                self.master.processed_image = image.copy()
+                self.master.image_viewer.show_image()
+                self.master.is_image_selected = True
+                
+    def save_button_released(self, event):
+        print("save action")
+
+    def save_as_button_released(self, event):
+        print("save as action")
+                
+    def clear_button_released(self, event):
+        print("Clear Images")
